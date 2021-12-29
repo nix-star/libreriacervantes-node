e0e396a472c5a7f96bdbe0f3cd25c180d9995558
Nicolás Chiesa <nicochiesa01@gmail.com>
2021-12-29 04:01:21 -0300

    finalizado

--- app.js
+++ app.js
@@ -4,15 +4,18 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+require('dotenv').config();
+var session = require('express-session');
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var destacadosRouter = require('./routes/destacados');
 var novedadesRouter = require('./routes/novedades');
 var busquedaRouter = require('./routes/busqueda');
+var loginRouter = require('./routes/admin/login');
+var adminNovedadesRouter = require('./routes/admin/novedades');
 
 var app = express();
-
-require('dotenv').config();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -24,11 +27,29 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(session({secret: 'keyboard cat',resave: false,saveUninitialized: true}));
+
+secured = async(req,res,next) =>{
+  try{
+    console.log(req.session.id_usuario);
+    if(req.session.id_usuario){
+      next()
+    } else{
+      res.redirect('/admin/login')
+    }
+  }catch(error){
+    console.log(error)
+  }
+}
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/destacados', destacadosRouter);
 app.use('/novedades', novedadesRouter);
 app.use('/busqueda', busquedaRouter);
+
+app.use('/admin/login', loginRouter);
+app.use('/admin/novedades', secured, adminNovedadesRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/admin/novedades.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+//var novedadesModel = require('../../models/novedadesModel');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    //var novedades = await novedadesModel.getNovedades();
+    res.render('admin/novedades', {
+      layout:'admin/layout',
+      usuario: req.session.nombre
+      //novedades
+    });
+});
+
+module.exports = router;
